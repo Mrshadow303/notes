2f93f46f5687663e374cafa32ed376864d96ada4
Mr_shadow <1226746535@qq.com>
2024-03-29 13:33:08 +0800

    message 5 ：the first iterative development

--- functional_test.py
+++ functional_test.py
@@ -2,7 +2,8 @@ from selenium import webdriver
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
-    def setup(self):
+
+    def setUp(self):
         self.browser = webdriver.Chrome()
 
     def tearDown(self):
@@ -14,9 +15,11 @@ class NewVisitorTest(unittest.TestCase):
         #张三听说有一个在线待办事项的应用
         #他去看了这个应用的首页
         self.browser.get('http://localhost:8000')
+
         #他注意到有to-do这个词
         self.assertIn('To-Do',self.browser.title),"browser title was:" + self.browser.title
         self.fail('Finish the test!')
+
         #应用有一个输入待办事项的文本输入框
         #他在文本输入框中输入了“Buy flowers"
 
