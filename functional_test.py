bede4dfa27de39b7a87c0155cdc17751785c141d
Mr_shadow <1226746535@qq.com>
2024-03-30 17:04:55 +0800

    message 8 : Before refactoring the functional_test

--- functional_test.py
+++ functional_test.py
@@ -30,28 +30,36 @@ class NewVisitorTest(unittest.TestCase):
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
-        )
+            )
 
         #他在文本输入框中输入了“Buy flowers"
         inputbox.send_keys('Buy flowers')
 
         #他按了回车键键后，页面更新了
-        #待办事项表格中显示了“1: Buy flowers
+        #待办事项表格中显示了“1: Buy flowers"
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         table = self.browser.find_element(By.ID,'id_list_table')
-        rows = table.find_element(By.TAG_NAME,'tr')
+        rows = table.find_elements(By.TAG_NAME,'tr')
         self.assertIn('1: Buy flowers',[row.text for row in rows])
 
         #页面中又显示了一个文本输入框，可以输入其他待办事项
-        #他输入了“gift to girlfriend"
-        self.fail('Finish the test!')
+        #他输入了“Give a gift to Lisi"
+        inputbox = self.browser.find_element(By.ID,'id_new_item')
+        inputbox.send_keys('Give a gift to Lisi')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         #页面再次更新，她的清单中显示了这两个待办事项
-        #张三想知道这个网站是否会记住他的清单
+        table = self.browser.find_element(By.ID,'id_list_table')
+        rows = table.find_elements(By.TAG_NAME,'tr')
+        self.assertIn('1: Buy flowers',[row.text for row in rows])
+        self.assertIn('2: Give a gift to Lisi',[row.text for row in rows])
 
+        #张三想知道这个网站是否会记住他的清单
         #他看到到网站为他生成了一个唯一的URL
+        self.fail('Finish the test!')
         #他访问那个URL
         #发现他的待办事项列表还在
         #他满意的离开了
