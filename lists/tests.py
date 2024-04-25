93e008f8b8bbab15f84f14577b5adadd96702bc0
Mr_shadow <1226746535@qq.com>
2024-04-25 16:04:22 +0800

    message 16 : form URL of new list by POST

--- lists/tests.py
+++ lists/tests.py
@@ -53,26 +53,11 @@ class ItemModelTest(TestCase):
         self.assertEqual(first_saved_item.text,'The first list item')
         self.assertEqual(second_saved_item.text,'Item the second')
 
-    def test_can_save_a_POST_request(self):
-        response = self.client.post('/',data={'item_text':'A new list item'})
-        
-        self.assertEqual(Item.objects.count(),1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text,'A new list item')
+    
 
-        
-
-        #self.assertIn('A new list item',response.content.decode())
-        #self.assertTemplateUsed(response,'home.html')
-
-    def test_redirects_after_POST(self):
-        response = self.client.post('/',data={'item_text':'A new list item'})
-        self.assertEqual(response.status_code,302)
-        self.assertEqual(response['location'],'/lists/the-new-page/')
-
-    def test_only_saves_items_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(),0)
+    # def test_only_saves_items_when_necessary(self):
+    #     self.client.get('/')
+    #     self.assertEqual(Item.objects.count(),0)
 
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
@@ -87,5 +72,24 @@ class ListViewTest(TestCase):
         # self.assertIn('itemey 2',response.content.decode())
         self.assertContains(response,'itemey 1')
         self.assertContains(response,'itemey 2')
+
+class NewListTest(TestCase):
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/lists/new',data={'item_text':'A new list item'})
+        
+        self.assertEqual(Item.objects.count(),1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text,'A new list item')
+
+        
+
+        #self.assertIn('A new list item',response.content.decode())
+        #self.assertTemplateUsed(response,'home.html')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/lists/new',data={'item_text':'A new list item'})
+        self.assertRedirects(response,'/lists/the-new-page/')
+        # self.assertEqual(response.status_code,302)
+        # self.assertEqual(response['location'],'/lists/the-new-page/')
 
 # Create your tests here.
