9fd9b5dcb73d2f3d46f0efcb723a237742d8bc50
Mr_shadow <1226746535@qq.com>
2024-05-09 15:22:37 +0800

    message 17: change models for multiple users

--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,7 @@ from django.urls import resolve
 from lists.views import home_page 
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-from lists.models import Item
+from lists.models import Item,List
 
 class HomePageTest(TestCase):
 
@@ -34,30 +34,30 @@ class HomePageTest(TestCase):
     #     self.assertTrue(html.endswith('</html>'))  #(4)
         
 
-class ItemModelTest(TestCase):
+# class ItemModelTest(TestCase):
 
-    def test_saving_and_retrieving_items(self):
-        first_item = Item()
-        first_item.text = 'The first list item'
-        first_item.save()
+#     def test_saving_and_retrieving_items(self):
+#         first_item = Item()
+#         first_item.text = 'The first list item'
+#         first_item.save()
 
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.save()
+#         second_item = Item()
+#         second_item.text = 'Item the second'
+#         second_item.save()
 
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(),2)
+#         saved_items = Item.objects.all()
+#         self.assertEqual(saved_items.count(),2)
 
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text,'The first list item')
-        self.assertEqual(second_saved_item.text,'Item the second')
+#         first_saved_item = saved_items[0]
+#         second_saved_item = saved_items[1]
+#         self.assertEqual(first_saved_item.text,'The first list item')
+#         self.assertEqual(second_saved_item.text,'Item the second')
 
     
 
-    # def test_only_saves_items_when_necessary(self):
-    #     self.client.get('/')
-    #     self.assertEqual(Item.objects.count(),0)
+#     # def test_only_saves_items_when_necessary(self):
+#     #     self.client.get('/')
+#     #     self.assertEqual(Item.objects.count(),0)
 
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
@@ -65,13 +65,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response,'list.html')
 
     def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-        response= self.client.get('/lists/the-new-page/')
-        # self.assertIn('itemey 1',response.content.decode())
-        # self.assertIn('itemey 2',response.content.decode())
-        self.assertContains(response,'itemey 1')
-        self.assertContains(response,'itemey 2')
+        list_user = List.objects.create()
+        Item.objects.create(text='itemey 1',list=list_user)
+        Item.objects.create(text='itemey 2',list=list_user)
 
 class NewListTest(TestCase):
     def test_can_save_a_POST_request(self):
@@ -92,4 +88,31 @@ class NewListTest(TestCase):
         # self.assertEqual(response.status_code,302)
         # self.assertEqual(response['location'],'/lists/the-new-page/')
 
+class ListAndItemModelsTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        list_user = List()
+        list_user.save()
+
+        first_item = Item()
+        first_item.text = 'The first list item'
+        first_item.list = list_user
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.list = list_user
+        second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list,list_user)
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(),2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text,'The first list item')
+        self.assertEqual(first_saved_item.list,list_user)
+        self.assertEqual(second_saved_item.text,'Item the second')
+        self.assertEqual(second_saved_item.list,list_user)
 # Create your tests here.
