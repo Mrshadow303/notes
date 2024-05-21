04cee54af9390562cb3e297f9c96d30b4a1deb41
Mr_shadow <1226746535@qq.com>
2024-05-21 21:24:15 +0800

    message 23:FT can test real_server

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -6,10 +6,18 @@ from selenium.webdriver.common.by import By
 #from django.test import LiveServerTestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.common.exceptions import WebDriverException
+import os
 
 MAX_WAIT = 10
 
 class NewVisitorTest(StaticLiveServerTestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        real_server = os.environ.get('REAL_SERVER')
+        if real_server:
+            self.live_server_url='http//'+real_server
+
+
     def test_layout_and_styling(self):
         #张三访问首页
         self.browser.get(self.live_server_url)
@@ -34,10 +42,6 @@ class NewVisitorTest(StaticLiveServerTestCase):
         )
 
 
-
-
-    def setUp(self):
-        self.browser = webdriver.Chrome()
 
     def tearDown(self):
         self.browser.quit()
